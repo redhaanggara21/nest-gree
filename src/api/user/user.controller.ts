ba2c3705bc0c7a838f6dd8825191e1cb45ab191f
redhaanggara21 <redhabayuanggara@gmail.com>
2022-11-18 12:56:25 +0700

    adding exception

--- src/api/user/user.controller.ts
+++ src/api/user/user.controller.ts
@@ -40,7 +40,7 @@ export class UserController {
   }
 
   @Get()
-  // @UseGuards(JwtAuthGuard)
+  @UseGuards(JwtAuthGuard)
   // @UseInterceptors(ClassSerializerInterceptor)
   @UseFilters(new HttpExceptionFilter())
   @ApiPaginatedResponse(CreateUserDto)

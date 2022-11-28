d88d7910480a1df10901b2bef743e24a596cbea2
redhaanggara21 <redhabayuanggara@gmail.com>
2022-11-28 15:29:42 +0700

    elastic search

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ApiModule } from './api/api.module';
 import { AppModule } from './app.module';
+import { AppoloModule } from './appolo/appolo.module';
 import { HttpExceptionFilter } from './common/exception-filter/http-exception.filter';
+import { DomainModule } from './domain/domain.module';
 import { setupSwagger } from './utill';
 
 async function bootstrap() {
@@ -17,7 +21,24 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter())
 
-  setupSwagger(app);
+  // setupSwagger(app);
+
+  const options = new DocumentBuilder()
+  .setTitle('API documents')
+  .setDescription('The API description')
+  .setVersion('1.0')
+  .addTag('ALL CONTROLLER')
+  .addBearerAuth()
+  .build();
+
+  const document = SwaggerModule.createDocument(app, options, {
+    include: [
+      ApiModule,
+      DomainModule,
+      AppoloModule
+    ],
+  });
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(port, () => {
     console.log('[WEB]', config.get<string>('BASE_URL'));
